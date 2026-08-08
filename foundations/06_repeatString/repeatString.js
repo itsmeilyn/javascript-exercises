ba5bae0b4d52f874950eff359b1e46c0242eed53
meilyn <119741026+itsmeilyn@users.noreply.github.com>
2026-08-08 19:40:39 +0700

    Submit: 06

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (str, n) {
+  if (n < 0) return "ERROR";
+  let string = "";
+  for (let i = 0; i < n; i++) {
+    string += str;
+  }
+  return string;
 };
 
 // Do not edit below this line
