c29d53a3d7748acc36fc859b28f3d66b2dd45d6b
meilyn <119741026+itsmeilyn@users.noreply.github.com>
2026-08-08 22:45:53 +0700

    fix bug: f8

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function (arr, ...items) {
-  return arr.filter((item) => !items.includes(item.name ?? item));
+const removeFromArray = function (arr, ...args) {
+  return arr.filter((item) => !args.includes(item));
 };
 
 console.log(removeFromArray([1, 2, 3, 4, 5], 2, 3));
