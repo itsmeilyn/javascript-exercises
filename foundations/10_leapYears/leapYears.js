ec395d47492dc2efc411778f45489f375299ef07
meilyn <119741026+itsmeilyn@users.noreply.github.com>
2026-08-08 23:42:48 +0700

    Submit: f10

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,12 +1,12 @@
 const leapYears = function (year) {
-  if (year % 4 === 0) {
+  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
     return true;
   } else {
-    false;
+    return false;
   }
 };
 
-const result = leapYears(2016);
+const result = leapYears(2100);
 console.log(result);
 
 // Do not edit below this line
