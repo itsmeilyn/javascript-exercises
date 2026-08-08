36a064dbe026738a6eb98372c70da599a1df892f
meilyn <119741026+itsmeilyn@users.noreply.github.com>
2026-08-08 23:38:34 +0700

    Start working on f-10

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  if (year % 4 === 0) {
+    return true;
+  } else {
+    false;
+  }
 };
+
+const result = leapYears(2016);
+console.log(result);
 
 // Do not edit below this line
 module.exports = leapYears;
