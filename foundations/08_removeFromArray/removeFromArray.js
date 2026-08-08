553df95f3c35f2a17ce02be3404e342af57b9ef1
meilyn <119741026+itsmeilyn@users.noreply.github.com>
2026-08-08 22:42:21 +0700

    Submit: f8

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...items) {
+  return arr.filter((item) => !items.includes(item.name ?? item));
 };
+
+console.log(removeFromArray([1, 2, 3, 4, 5], 2, 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
