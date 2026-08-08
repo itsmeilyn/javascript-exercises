2508df31a5f94a08097bb1038adb9f095a1038c1
meilyn <119741026+itsmeilyn@users.noreply.github.com>
2026-08-08 23:20:42 +0700

    Submit: f9

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+  //   if (typeof start !== "number" || typeof end !== "number") {
+  //     return "ERROR";
+  //   }
+  if (!Number.isInteger(start) || !Number.isInteger(end)) {
+    return "ERROR";
+  }
+  if (start < 0 || end < 0) {
+    return "ERROR";
+  }
 
+  let total = 0;
+  if (start > end) {
+    for (let i = start; i >= 0; i--) {
+      total += i;
+    }
+  } else {
+    for (let i = start; i <= end; i++) {
+      total += i;
+    }
+  }
+  return total;
 };
+
+console.log(sumAll(1, 2));
 
 // Do not edit below this line
 module.exports = sumAll;
